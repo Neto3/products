a337481d6ec38b3766c1de82c43e1688a827e04a
Neto3 <45779772+Neto3@users.noreply.github.com>
2021-05-16 21:31:02 -0300

    update dev db settings

--- fuel/app/config/development/db.php
+++ fuel/app/config/development/db.php
@@ -22,9 +22,9 @@
 return array(
 	'default' => array(
 		'connection' => array(
-			'dsn'      => 'mysql:host=localhost;dbname=fuel_dev',
+			'dsn'      => 'mysql:host=localhost;dbname=products_dev',
 			'username' => 'root',
-			'password' => 'root',
+			'password' => '',
 		),
 	),
 );
